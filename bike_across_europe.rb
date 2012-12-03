069dcef85b57bcf3c91156f213ed5c368d410b1e
Jack Danger <jackdanger@squareup.com>
2012-12-02 21:33:14 -0800

    Ruby impl with 3 attempts

---
+++ bike_across_europe.rb
@@ -0,0 +1,197 @@
+module Util
+  RADIANS = (180/3.14169);
+  def self.kilometersBetween(lat1, lng1, lat2, lng2)
+    a1 = lat1 / RADIANS
+    a2 = lng1 / RADIANS
+    b1 = lat2 / RADIANS
+    b2 = lng2 / RADIANS
+
+    t1 = Math.cos(a1) * Math.cos(a2) * Math.cos(b1) * Math.cos(b2)
+    t2 = Math.cos(a1) * Math.sin(a2) * Math.cos(b1) * Math.sin(b2)
+    t3 = Math.sin(a1) * Math.sin(b1)
+
+    6366 * Math.acos(t1 + t2 + t3)
+  end
+end
+
+class City
+
+  attr_reader :lat, :lng
+
+  def initialize(name, lat, lng)
+    @name, @lat, @lng = name, lat, lng
+  end
+
+  Berlin    = new('Berlin',    52.482668, 13.359275)
+  Paris     = new('Paris',     48.980405, 2.2851849)
+  Milan     = new('Milan',     45.520543, 9.1419459)
+  Frankfurt = new('Frankfurt', 50.078848, 8.6349115)
+  Munich    = new('Munich',    48.166229, 11.558089)
+  Zurich    = new('Zurich',    47.383444, 8.5254142)
+  Tours     = new('Tours',     47.413572, 0.6810506)
+  Lyon      = new('Lyon',      45.767122, 4.8339568)
+  Vienna    = new('Vienna',    48.224431, 16.389240)
+  Prague    = new('Prague',    50.092396, 14.436144)
+  Krakow    = new('Krakow',    50.050363, 19.928578)
+  Warsaw    = new('Warsaw',    52.254756, 21.005968)
+  Hamburg   = new('Hamburg',   53.539699, 9.9977143)
+  Antwerp   = new('Antwerp',   51.220613, 4.3954882)
+  Torino    = new('Torino',    45.105321, 7.6451957)
+  Rome      = new('Rome',      42.032845, 12.390408)
+
+  def kilometersTo(other)
+    Util.kilometersBetween(lat, lng, other.lat, other.lat)
+  end
+
+  def roads
+    @roads ||= Road::ALL.select {|road| road.a == self || road.b == self }
+  end
+
+  def adjacent_cities
+    roads.map {|road| road.the_city_opposite(self) }
+  end
+
+  def to_s
+    @name
+  end
+end
+
+class Ride
+  attr_reader :road, :from, :to
+
+  def initialize(from, to)
+    @from, @to = from, to
+    @road = Road.between(from, to)
+  end
+
+  def to_s
+    "#{from} -> #{to} (#{"%.0f" % road.distance} km)"
+  end
+end
+
+class Road
+
+  require 'set'
+  ALL = Set.new
+
+  attr_reader :a, :b, :distance
+
+  def initialize(a, b)
+    raise "Roads must have two cities" if a == b
+    @a = a
+    @b = b
+    @distance = a.kilometersTo(b)
+  end
+
+  def the_city_opposite(city)
+    raise "#{city} isn't connected to #{self}" unless a == city || b == city
+    city == a ? b : a
+  end
+
+  def to_s
+    "#{a} | #{b} (#{"%.0f" % distance} km)"
+  end
+
+  def self.between(a, b)
+    ALL.detect do |road|
+      return road if (road.a == a && road.b == b) || (road.a == b && road.b == a)
+    end
+    raise "There is no road between " + a.to_s + " and " + b.to_s
+  end
+
+  ALL.add new(City::Hamburg,   City::Berlin)
+  ALL.add new(City::Hamburg,   City::Antwerp)
+  ALL.add new(City::Hamburg,   City::Frankfurt)
+  ALL.add new(City::Berlin,    City::Warsaw)
+  ALL.add new(City::Berlin,    City::Prague)
+  ALL.add new(City::Antwerp,   City::Paris)
+  ALL.add new(City::Paris,     City::Tours)
+  ALL.add new(City::Paris,     City::Lyon)
+  ALL.add new(City::Paris,     City::Zurich)
+  ALL.add new(City::Paris,     City::Frankfurt)
+  ALL.add new(City::Frankfurt, City::Prague)
+  ALL.add new(City::Krakow,    City::Warsaw)
+  ALL.add new(City::Krakow,    City::Prague)
+  ALL.add new(City::Krakow,    City::Vienna)
+  ALL.add new(City::Vienna,    City::Munich)
+  ALL.add new(City::Vienna,    City::Prague)
+  ALL.add new(City::Zurich,    City::Milan)
+  ALL.add new(City::Lyon,      City::Torino)
+  ALL.add new(City::Torino,    City::Milan)
+  ALL.add new(City::Torino,    City::Rome)
+  ALL.add new(City::Milan,     City::Rome)
+end
+
+Start = City::Rome
+End   = City::Berlin
+
+class GreedyRandomDepthFirstWithLoops1
+  def run
+    travel(Start)
+  end
+
+  def travel(city, path = [])
+    return path if city == End
+    next_city = city.adjacent_cities.sample
+    travel(next_city, path + [Ride.new(city, next_city)])
+  end
+end
+
+class GreedyRandomDepthFirstWithoutLoops2
+  attr_reader :visited
+  def run
+    @visited = Set.new
+    travel(Start)
+  end
+
+  def travel(city, path = [])
+    visited << city
+    return path if city == End
+    city.adjacent_cities.reject {|c| visited.include?(c) }.each do |next_city|
+      if result = travel(next_city, path + [Ride.new(city, next_city)])
+        return result
+      end
+    end
+    nil
+  end
+end
+
+class GreedyRandomBreadthFirstWithoutLoops3
+  attr_reader :visited
+  def run
+    @visited = Set.new
+    travel
+  end
+
+  class Node
+    attr_accessor :city, :path
+    def initialize(city, path)
+      @city, @path = city, path
+    end
+  end
+
+  def travel
+    frontier = [Node.new(Start, [])]
+    while frontier.any?
+      node = frontier.shift
+      return node.path if node.city == End
+      visited << node.city
+      node.city.adjacent_cities.shuffle.each do |adjacent|
+        ride = Ride.new(node.city, adjacent)
+        frontier << Node.new(adjacent, node.path + [ride]) unless visited.include? adjacent
+      end
+    end
+  end
+end
+
+
+#result = GreedyRandomDepthFirstWithLoops1.new.run
+#result = GreedyRandomDepthFirstWithoutLoops2.new.run
+result = GreedyRandomBreadthFirstWithoutLoops3.new.run
+
+
+
+result.each {|ride| puts ride }
+print "arrived in #{result.size} steps "
+print "(#{"%.0f" % result.map(&:road).map(&:distance).inject(&:+)} km)"
+puts ""
--- src/BikeAcrossEurope.java
+++ src/BikeAcrossEurope.java
@@ -1,7 +1,9 @@
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 
 /**
  * Created with IntelliJ IDEA. User: jackdanger Date: 11/27/12 Time: 7:33 PM To change this template
@@ -41,26 +43,34 @@ public class BikeAcrossEurope {
     return path;
   }
 
+  public static List<Ride> attempt1_stack(List<Ride> path)
+
   /*
-    Depth-first while checking for loops
+    random depth-first while checking for backtracking
    */
   public static List<Ride> attempt2() {
     List<Ride> path = new ArrayList<Ride>();
-    Map<City> visited = new HashMap<City>();
+    Set<City> visited = new HashSet<City>();
 
     City current = start;
     while (current != end) {
       System.out.println(current);
       List<Road> nextRoads = new ArrayList<Road>();
       for (Road road : Road.all) {
-        if (road.a == current || road.b == current){
-          nextRoads.add(road);
+        if (road.a == current && road.b == current){
+          City nextCity = road.a == current ? road.b : road.a;
+          if (!visited.contains(nextCity))
+            nextRoads.add(road);
         }
       }
-      // choose at random, don't pick a visited one
-      Road nextRoad = nextRoads.get((int) Math.floor(Math.random() * nextRoads.size()));
-      current = nextRoad.a == current ? nextRoad.b : nextRoad.a;
-      path.add(new Ride(nextRoad, current));
+      if (nextRoads.isEmpty()) {
+        current = path.remove(path.size()-1).to;
+      } else {
+        // choose at random, don't pick a visited one
+        Road nextRoad = nextRoads.get((int) Math.floor(Math.random() * nextRoads.size()));
+        current = nextRoad.a == current ? nextRoad.b : nextRoad.a;
+        path.add(new Ride(nextRoad, current));
+      }
     }
     return path;
   }
