66d80434b27b046f99670c9faf08c5289b5133a1
Jack Danger <jackdanger@squareup.com>
2012-12-02 23:09:54 -0800

    preparing for adding more data

---
+++ prepare_wiki.rb
@@ -0,0 +1,11 @@
+#//alert( "d:"+d+" m:"+m+" s:"+s );
+lines = File.read('latlng-world.wiki').split("\n")
+lines.each do |line|
+  lat, lng, city, state, country = line.split(', ')
+  p city
+  sign = 
+d = d-0;
+m = m-0;
+end
+#var sign = ( dir=='W' || dir==alert'S' ) ? -1 : 1;
+return (((s/60+m)/60)+d)*sign;sign
---
+++ src/GreedyRandomDepthFirstWithLoops1.java
@@ -0,0 +1,6 @@
+/**
+ * Created with IntelliJ IDEA. User: jackdanger Date: 12/2/12 Time: 10:38 PM To change this template
+ * use File | Settings | File Templates.
+ */
+public class GreedyRandomDepthFirstWithLoops1 {
+}
